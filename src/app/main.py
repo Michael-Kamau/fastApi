d0465491ea9dd6f16c116eab8e6d05895736580a
Michael Kamau <kamau.karitu@gmail.com>
2024-01-14 21:07:38 +0300

    PROJECT SETUP - setup fast api project - setup docker

---
+++ src/app/main.py
@@ -0,0 +1,28 @@
+from typing import Union
+
+from fastapi import FastAPI
+from fastapi.params import Body
+from pydantic import BaseModel
+from typing import Optional
+
+app = FastAPI()
+
+class Post(BaseModel):
+    title: str
+    content: str
+    published: bool = True
+    rating: Optional[int] = None
+
+@app.get("/")
+def read_root():
+    return {"Hello": "World"}
+
+
+@app.get("/items/{item_id}")
+def read_item(item_id: int, q: Union[str, None] = None):
+    return {"item_id": item_id, "q": q}
+
+@app.post('/posts')
+def createPost(post: Post):
+    print(post)
+    return {'message':'Successfully added the comment'}
