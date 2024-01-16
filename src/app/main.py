7d4cffb4c59ae93d016c47074cfe12582cc20e0d
Michael Kamau <kamau.karitu@gmail.com>
2024-01-16 17:39:00 +0300

    Pydantic models for request and response

--- src/app/main.py
+++ src/app/main.py
@@ -3,10 +3,12 @@ from typing import Union
 from fastapi import FastAPI, Depends, status, HTTPException, Response
 from fastapi.params import Body
 from pydantic import BaseModel
-from typing import Optional
+from typing import Optional, List
 from sqlalchemy.orm import Session
 
 from . import models
+from . import schemas
+
 
 from .database import  engine, get_db
 
@@ -15,34 +17,27 @@ models.Base.metadata.create_all(bind=engine)
 app = FastAPI()
 
 
-
-class Post(BaseModel):
-    title: str
-    content: str
-    published: bool = True
-    # rating: Optional[int] = None
-
 @app.get("/")
 def read_root():
     return {"Hello": "World"}
 
 
-@app.get("/posts")
+@app.get("/posts", response_model=List[schemas.Post])
 def get_posts(db:Session = Depends(get_db)):
     posts = db.query(models.Post).all()
-    return {"data": posts}
+    return  posts
 
-@app.get("/posts/{id}")
+@app.get("/posts/{id}", response_model=schemas.Post)
 def get_post(id: int,db: Session=Depends(get_db)):
 
     post = db.query(models.Post).filter(models.Post.id == id).first()
     if post == None:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"Post with id {id} was not found")
-    print(post)
-    return {"data": post}
+    
+    return  post
 
-@app.post('/posts', status_code=status.HTTP_201_CREATED)
-def createPost(post: Post, db: Session = Depends(get_db)):
+@app.post('/posts', status_code=status.HTTP_201_CREATED, response_model= schemas.Post)
+def createPost(post: schemas.PostCreate, db: Session = Depends(get_db)):
     new_post = models.Post(**post.dict())
 
     print(post)
@@ -51,7 +46,7 @@ def createPost(post: Post, db: Session = Depends(get_db)):
     db.add(new_post)
     db.commit()
     db.refresh(new_post)
-    return {'message':'Successfully added the post', 'data':new_post}
+    return new_post
 
 @app.delete('/posts/{id}')
 def delete_post(id:int, db:Session=Depends(get_db)):
@@ -65,8 +60,8 @@ def delete_post(id:int, db:Session=Depends(get_db)):
 
     return Response(status_code=status.HTTP_204_NO_CONTENT)
 
-@app.put('/posts/{id}')
-def update_post(id:int, updated_post:Post, db:Session=Depends(get_db)):
+@app.put('/posts/{id}', response_model=schemas.Post)
+def update_post(id:int, updated_post:schemas.PostCreate, db:Session=Depends(get_db)):
     post_query= db.query(models.Post).filter(models.Post.id == id)
 
     post = post_query.first()
@@ -78,10 +73,5 @@ def update_post(id:int, updated_post:Post, db:Session=Depends(get_db)):
 
     db.commit()
 
-    return {'data': post_query.first()}
+    return post_query.first()
 
-
-@app.get("/sqlalchemy")
-def test_post(db:Session = Depends(get_db)):
-    posts = db.query(models.Post).all()
-    return {"data": posts}
